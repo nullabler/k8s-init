ca83290ba83dfc9a9d91f47abd829db0633f9eb7
nix <mail@tmake.ru>
2023-12-27 11:52:19 +0200

    remove firewall

--- master.sh
+++ master.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
+# Init k8s
 sudo kubeadm init
 
+# Copy config for kubectl
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo cp -i /etc/kubernetes/admin.conf /root/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-sudo ufw allow 6443
-sudo ufw allow 6443/tcp
-sudo ufw enable
-
+# Install CNI Cilium
 CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
 CLI_ARCH=amd64
 if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
@@ -22,4 +21,12 @@ rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
 sudo cilium install
 sudo cilium hubble enable
 
+# Install helm
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+
+# Show token for worker nodes
 sudo kubeadm token create --print-join-command
